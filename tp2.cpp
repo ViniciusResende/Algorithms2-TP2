b22dfdffca494143d0f6a5dcd0b9ac0111696acd
Vinicius Faria <viniciusfariaresende@gmail.com>
2023-12-07 15:28:25 -0300

    feat: improving time tracking

--- tp2.cpp
+++ tp2.cpp
@@ -56,14 +56,12 @@ std::vector<std::tuple<float, float>> read_tsp_file_input(std::string file_path)
   return points;
 }
 
-void print_minutes_and_second(long long microseconds) {
-  int minutes = microseconds / 60000000;
-  int seconds = (microseconds % 60000000) / 1000000;
-  if(seconds > 0) {
-    std::cout << "Execution time: " << minutes << " minutes and " << seconds << " seconds." << std::endl;
-  } else {
-    std::cout << "Execution time: " << microseconds << " microseconds." << std::endl;
-  }
+void print_minutes_and_second(long seconds) {
+  seconds %= 3600;
+  int minutes = seconds / 60;
+  seconds %= 60;
+
+  std::cout << "Execution time: " << minutes << " minutes and " << seconds << " seconds." << std::endl;
 }
 
 /**
@@ -76,13 +74,7 @@ void print_minutes_and_second(long long microseconds) {
  * @return 0 indicating successful execution of the program.
  */
 int main() {
-  auto start = std::chrono::high_resolution_clock::now();
-
   std::vector<std::tuple<float, float>> points = read_tsp_file_input(FILE_PATH);
-  // Print the points
-  // for (const auto& point : points) {
-  //   std::cout << "Point: (" << std::get<0>(point) << ", " << std::get<1>(point) << ")" << std::endl;
-  // }
 
   // Create a matrix
   // std::vector<std::vector<float>> matrix(points.size(), std::vector<float>(points.size()));
@@ -95,6 +87,7 @@ int main() {
       {20, 25, 30, 0}});
 
   // Approximate TSP
+  auto start_approx = std::chrono::high_resolution_clock::now();
   std::vector<int> walk_approx = approximate_tsp(matrix);
 
   // Print the walk
@@ -106,7 +99,12 @@ int main() {
   std::cout << "]" << std::endl;
   std::cout << "Weight: " << calculate_path_weight(matrix, walk_approx) << std::endl;
 
+  auto stop_approx = std::chrono::high_resolution_clock::now();
+  auto duration_approx = std::chrono::duration_cast<std::chrono::seconds>(stop_approx - start_approx);
+  print_minutes_and_second(duration_approx.count());
+
   // Christofides TSP
+  auto start_chris = std::chrono::high_resolution_clock::now();
   std::vector<int> walk_christofides = christofides_tsp(matrix);
 
   // Print the walk
@@ -118,9 +116,13 @@ int main() {
   std::cout << "]" << std::endl;
   std::cout << "Weight: " << calculate_path_weight(matrix, walk_christofides) << std::endl;
 
+  auto stop_chris = std::chrono::high_resolution_clock::now();
+  auto duration_chris = std::chrono::duration_cast<std::chrono::seconds>(stop_chris - start_chris);
+  print_minutes_and_second(duration_chris.count());
+
   // BNB TSP
+  auto start_bnb = std::chrono::high_resolution_clock::now();
   std::vector<int> walk_bnb = branchAndBound(matrix);
-  // std::vector<int> walk_bnb = {0, 13, 12, 11, 6, 5, 14, 4, 10, 8, 9, 18, 19, 20, 15, 2, 1, 16, 21, 3, 17, 7, 0};
 
   // Print the walk
   std::cout << "Branch and Bound TSP Algorithm: " << std::endl;
@@ -131,9 +133,9 @@ int main() {
   std::cout << "]" << std::endl;
   std::cout << "Weight: " << calculate_path_weight(matrix, walk_bnb) << std::endl;
 
-  auto stop = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-
-  print_minutes_and_second(duration.count());
+  auto stop_bnb = std::chrono::high_resolution_clock::now();
+  auto duration_bnb = std::chrono::duration_cast<std::chrono::microseconds>(stop_bnb - start_bnb);
+  print_minutes_and_second(duration_chris.count());
+  
   return 0;
 }
